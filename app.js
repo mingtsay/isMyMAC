8c8a62300b9252ec309cb270cb5501fb818f779c
rsghost <im@rsghost.org>
2014-05-03 02:51:35 +0800

    update

--- app.js
+++ app.js
@@ -6,32 +6,24 @@ var express = require('express'),
     http = require('http'),
     path = require('path');
 
-var app = module.exports = express();
+var app = module.exports = express(),
+    config = require('./config.json');
 
 // Configuration
 app.configure(function () {
-  app.set('port', process.env.PORT || 3001);
-  app.use(express.favicon());
+  app.set('port', process.env.PORT || config.port);
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
 });
 
-app.configure('development', function () {
-  app.use(express.errorHandler());
-});
-
 // Router
-var config = require('./config.json');
-app.get('/GETMAC', function (req, res) {
-  var exec = require('child_process').exec,
-      child;
-
-  child = exec(config.command, function (error, stdout, stderr) {
-    if (error !== null) {
-      console.log('[ERROR] exec error: ' + error);
-      console.log('[ERROR] stderr: ' + stderr);
+app.get('/', function (req, res) {
+  var exec = require('child_process').exec;
+  var child = exec(config.command, function (err, stdout) {
+    if (err !== null) {
+      console.log('[ERROR] ' + err);
     }
     var ip = '(' + req.ip + ')';
     stdout = stdout.split("\n");
@@ -46,6 +38,7 @@ app.get('/GETMAC', function (req, res) {
     if (!ans) {
       var ans = 'notfound';
     }
+
     res.json({
       result: [req.ip, ans]
     });
